c96756c35cf84c25fce06a558a2ff13799300782
Brandon Johnson <77591826+brandon121j@users.noreply.github.com>
2022-12-07 17:58:11 -0500

    Add project files.

---
+++ AD Unlocker/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace AD_Unlocker
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(12, 89);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(283, 259);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick_1);
+            this.listBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.listBox1_KeyPress);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(283, 23);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Location = new System.Drawing.Point(12, 366);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(283, 59);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Unlock User";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(307, 27);
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.AutoSize = false;
+            this.toolStripButton1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Lavender;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Overflow = System.Windows.Forms.ToolStripItemOverflow.Never;
+            this.toolStripButton1.Size = new System.Drawing.Size(24, 24);
+            this.toolStripButton1.Text = "toolStripButton1";
+            this.toolStripButton1.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.toolStripButton1.ToolTipText = "Refresh";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            this.toolStripButton1.MouseEnter += new System.EventHandler(this.toolStripButton1_MouseEnter);
+            this.toolStripButton1.MouseLeave += new System.EventHandler(this.toolStripButton1_MouseLeave);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(307, 445);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "AD Unlocker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+    }
+}
+
---
+++ AD Unlocker/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.DirectoryServices;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AD_Unlocker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // List that contains data that's displayed in listbox
+        List<string> adUsers = new List<string>();
+
+        // Gets Active Directory Path
+        private string GetCurrentDomainPath()
+        {
+            DirectoryEntry de = new DirectoryEntry("LDAP://RootDSE");
+
+            return "LDAP://" + de.Properties["defaultNamingContext"][0].ToString();
+        }
+
+        // Adds ability to search listbox for specific value
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            listBox1.SelectedIndex = listBox1.FindString(textBox1.Text);
+        }
+
+        // When the Unlock User button is pressed 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            UserUnlocker();
+        }
+
+        // Sets Enter name: placeholder on form load and populates listbox
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.PlaceholderText = "Enter name";
+            LockedUsersQuery();
+        }
+
+        // Populates listbox with list of all locked users
+        private void LockedUsersQuery()
+        {
+            Cursor.Current = Cursors.WaitCursor;
+
+            adUsers.Clear();
+
+            var searcher = new DirectorySearcher()
+            {
+                Filter = "(&(objectCategory=person)(objectClass=user)(lockoutTime>=1)(!(userAccountControl:1.2.840.113556.1.4.803:=2)))",
+                PageSize = 1000,
+                PropertiesToLoad = { "sAMAccountName" }
+            };
+
+            using (var results = searcher.FindAll())
+            {
+
+                foreach (SearchResult result in results)
+                {
+                    var username = result.Properties["sAMAccountName"][0].ToString();
+
+                    adUsers.Add(username);
+                }
+            }
+
+            ResultsUpdater();
+
+            Cursor.Current = Cursors.Default;
+        }
+
+        // Displays additional user info when Enter key is pressed in textbox
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                AdditionalInfo();
+            }
+
+        }
+
+        // Function for unlocking user
+        private void UserUnlocker()
+        {
+            try
+            {
+                string selectedUser = listBox1.SelectedItem.ToString();
+
+                DirectoryEntry userEntry = new DirectoryEntry(GetCurrentDomainPath());
+
+                DirectorySearcher search = new DirectorySearcher(userEntry);
+
+                search.Filter = $"(&(objectCategory=person)(objectClass=user)(sAMAccountName={listBox1.SelectedItem}))";
+
+                SearchResult user = search.FindOne();
+
+                DirectoryEntry userInfo = user.GetDirectoryEntry();
+
+                userInfo.Properties["LockOutTime"].Value = 0;
+
+                userInfo.CommitChanges();
+
+                userInfo.Close();
+
+                userEntry.Close();
+
+                LockedUsersQuery();
+
+                MessageBox.Show("Success!", $"{selectedUser}'s account has been unlocked!");
+            }
+            catch
+            {
+                MessageBox.Show("Error!", "An Error has occured");
+
+            }
+        }
+
+        // Updates Listbox to reflect changes
+        private void ResultsUpdater()
+        {
+            listBox1.Items.Clear();
+
+            foreach (var item in adUsers)
+            {
+                listBox1.Items.Add(item.ToString());
+            }
+
+            listBox1.SelectedItem = listBox1.Items[0];
+
+        }
+
+
+        // Pulls up additional user data
+        private void listBox1_DoubleClick_1(object sender, EventArgs e)
+        {
+            AdditionalInfo();
+        }
+
+        // Displays users full name and email
+        private void AdditionalInfo()
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                try
+                {
+
+                    DirectorySearcher search = new DirectorySearcher(GetCurrentDomainPath());
+
+                    search.Filter = $"(&(objectCategory=person)(objectClass=user)(sAMAccountName={listBox1.SelectedItem}))";
+
+                    SearchResult result = search.FindOne();
+
+                    if (result != null)
+                    {
+
+                        string displayName = result.Properties["displayName"][0].ToString();
+
+                        string email = result.Properties["mail"][0].ToString();
+
+                        var newline = System.Environment.NewLine;
+
+                        StringBuilder message = new StringBuilder();
+
+                        message.Append(displayName + newline + email);
+
+                        string title = "Unlock this user?";
+
+                        MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+
+                        DialogResult dr = MessageBox.Show(message.ToString(), title, buttons);
+
+                        if (dr == DialogResult.Yes)
+                        {
+                            UserUnlocker();
+
+                        }
+
+                    }
+                }
+                catch { }
+            }
+        }
+
+        // Makes Enter key display additional user information
+        private void listBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                AdditionalInfo();
+            }
+        }
+
+        // Allows user to navigate listbox through textbox with up/down keys
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                if (listBox1.SelectedIndex > 0)
+                    this.listBox1.SelectedIndex--;
+                e.Handled = true;
+
+            }
+            else if (e.KeyCode == Keys.Down )
+            {
+                if (listBox1.SelectedIndex < (listBox1.Items.Count - 1))
+                   this.listBox1.SelectedIndex++;
+                e.Handled = true;
+            }
+        }
+
+        // Refreshes data when refresh button is pressed
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+
+            LockedUsersQuery();
+
+        }
+
+        // Changes cursor when hovering over refresh button
+        private void toolStripButton1_MouseEnter(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.Hand;
+        }
+
+        private void toolStripButton1_MouseLeave(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.Default;
+        }
+    }
+}
+
